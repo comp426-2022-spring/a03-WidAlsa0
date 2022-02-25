d727830b1bf96c3ae5e71e685d908d64e11f06f1
WidAlsa0 <69405330+WidAlsa0@users.noreply.github.com>
2022-02-25 11:51:39 -0500

    added echo

--- server.js
+++ server.js
@@ -7,6 +7,16 @@ const server = app.listen(port, () => {
     console.log('App is running on port %port%.'.replace('%port%',port))
 })
 
+app.get('./app',(req, res) => {
+    res.status(200).end('OK')
+    res.type('text/plain')
+    res.end(res.statusCode+''+res.statusMessage)
+})
+
+app.get('/app/echo/:number', (req, res) => {
+    res.status(200).json({'message': req.params.number })
+})
+
 app.use(function(req, res) {
     res.status(404).send("Endpoint does not exist")
     res.type("text/plain")
