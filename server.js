2729343f69268a82250afc8dd02559e3d78a1268
WidAlsa0 <69405330+WidAlsa0@users.noreply.github.com>
2022-02-25 12:02:26 -0500

    added coin flip endpoint

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const server = app.listen(port, () => {
     console.log('App is running on port %port%.'.replace('%port%',port))
 })
 
+function coinFlip() {
+    return (Math.floor(Math.random() * 2) == 0) ? 'heads' : 'tails';
+}
+
 app.get('./app',(req, res) => {
     res.status(200).end('OK')
     res.type('text/plain')
@@ -15,6 +19,11 @@ app.get('./app',(req, res) => {
 
 app.get('/app/echo/:number', (req, res) => {
     res.status(200).json({'message': req.params.number })
+})
+
+app.get('/app/flip', (req, res) => {
+    var flip = coinFlip()
+    res.status(200).json({'flip': flip})
 })
 
 app.use(function(req, res) {
