e4c07dd72a3d2edd7c8c1728c34245740732191a
WidAlsa0 <69405330+WidAlsa0@users.noreply.github.com>
2022-02-25 12:39:41 -0500

    removed res.net

--- server.js
+++ server.js
@@ -38,33 +38,28 @@ app.get('./app',(req, res) => {
 
 app.get('/app/echo/:number', (req, res) => {
     res.status(200).json({'message': req.params.number })
-    res.end(res.statusCode+''+res.statusMessage)
 })
 
 app.get('/app/flip', (req, res) => {
     var flip = coinFlip()
     res.status(200).json({'flip': flip})
-    res.end(res.statusCode+''+res.statusMessage)
 })
 
 app.get('/app/flips/:number', (req, res) => {
     const flips = coinFlips(req.params.number)
     res.status(200).json({'flips': flips})
-    res.end(res.statusCode+''+res.statusMessage)
 })
 
 app.get('/app/flip/call/heads', (req, res) => {
     var input = "heads"
     const flips = flipACoin(input)
     res.status(200).json(flips)
-    res.end(res.statusCode+''+res.statusMessage)
 })
 
 app.get('/app/flip/call/tails', (req, res) => {
     var input = "tails"
     const flips = flipACoin(input)
     res.status(200).json(flips)
-    res.end(res.statusCode+''+res.statusMessage)
 })
 
 app.use(function(req, res) {
