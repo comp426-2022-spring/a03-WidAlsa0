215e33afdc42090aa479fed00fe9233a4c5cead7
WidAlsa0 <69405330+WidAlsa0@users.noreply.github.com>
2022-04-07 17:06:40 -0400

    order swap on ports

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const args = require("minimist")(process.argv.slice(2))
 args["port"]
 const app = express()
 
-var port = 5000 || process.env.PORT || args.port
+var port = args.port || process.env.PORT || 5000
 
 const server = app.listen(port, () => {
     console.log('App is running on port %port%.'.replace('%port%',port))
